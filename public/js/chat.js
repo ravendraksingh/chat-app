5c4130069e568ee4f9b94911bda811fcfa6bee91
ravendraksingh@gmail.com <ravendraksingh@gmail.com>
2020-07-21 13:38:38 +0530

    added more code

--- public/js/chat.js
+++ public/js/chat.js
@@ -4,18 +4,69 @@ const socket = io()
 const $messageForm = document.querySelector('#message-form')
 const $messageFormInput = $messageForm.querySelector('input')
 const $messageFormButton = $messageForm.querySelector('button')
-
 const $sendLocationButton = document.querySelector('#send-location')
+const $messages = document.querySelector('#messages')
+
+// Templates 
+const messageTemplate = document.querySelector('#message-template').innerHTML
+const locationMessageTemplate = document.querySelector('#location-message-template').innerHTML
+const sidebarTemplate = document.querySelector('#sidebar-template').innerHTML
+
+// Options
+const {username, room} = Qs.parse(location.search, { ignoreQueryPrefix: true })
+
+const autoscroll = () => {
+    // New message element
+    const $newMessage = $messages.lastElementChild
+
+    // Height of the message 
+    const newMessageStyle = getComputedStyle($newMessage)
+    const newMesageMargin = parseInt(newMessageStyle.marginBottom)
+    const newMessageHeight = $newMessage.offsetHeight + newMesageMargin
+
+    // Visible height
+    const visibleHeight = $messages.offsetHeight
+    // Height of message container
+    const containerHeight = $messages.scrollHeight
+    // How far have I scrolled
+    const scrollOffset = $messages.scrollTop + visibleHeight
+
+    if (containerHeight - newMessageHeight <= scrollOffset) {
+        $messages.scrollTop = $messages.scrollHeight
+    }
+}
 
 socket.on('message', (message) => {
     console.log(message)
+    const html = Mustache.render(messageTemplate, {
+        username: message.username,
+        message: message.text,
+        createdAt: moment(message.createdAt).format('h:mm a')
+    })
+    $messages.insertAdjacentHTML('beforeend', html)
+    autoscroll()
 })
 
-// document.querySelector('#increment').addEventListener('click', () => {
-//     console.log('clicked')
-//     socket.emit('increment')
-//     socket.emit('increment')
-// })
+socket.on('locationMessage', (message) => {
+    console.log(message)
+    const html = Mustache.render(locationMessageTemplate, {
+        username: message.username,
+        url: message.url,
+        createdAt: moment(message.createdAt).format('h:mm a')
+    })
+    $messages.insertAdjacentHTML('beforeend', html)
+    autoscroll()
+})
+
+
+socket.on('roomData', ({ room, users }) => {
+    const html = Mustache.render(sidebarTemplate, {
+        room, 
+        users
+    })
+    document.querySelector('#sidebar').innerHTML = html
+})
+
 
 $messageForm.addEventListener('submit', (e) => {
     e.preventDefault()
@@ -24,9 +75,7 @@ $messageForm.addEventListener('submit', (e) => {
 
     const message = e.target.elements.message.value
 
-    //socket.emit('sendMessage', message)
     socket.emit('sendMessage', message, (error) => {
-        //console.log('the message was delivered', error)
         $messageFormButton.removeAttribute('disabled')
         $messageFormInput.value = ''
         $messageFormInput.focus()
@@ -34,7 +83,6 @@ $messageForm.addEventListener('submit', (e) => {
         if (error) {
             return console.log(error)
         } 
-
         console.log('Mesaage delivered!')
     })
 })
@@ -57,4 +105,12 @@ $sendLocationButton.addEventListener('click', (e) => {
             console.log('Location shared!')
         })
     })
+})
+
+
+socket.emit('join', { username, room }, (error) => {
+    if (error) {
+        alert(error)
+        location.href = "/"
+    }
 })
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
 const Filter = require('bad-words')
+const { generateMessage, generateLocationMessage } = require('./utils/messages')
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./utils/users')
 
 
 const app = express()
@@ -16,36 +18,60 @@ app.use(express.static(publicDirectoryPath))
 
 let count = 0
 
-// server (emits) => client(receive) - countUpdated
-// client (emits) => server(receive) - increment
-
-
 io.on('connection', (socket) => {
     console.log('New WebSocket connection')
+    
+    socket.on('join', (options, callback) => {
+        const { error, user } =  addUser({ id: socket.id, ...options })
 
-    socket.emit('message', 'Welcome!')
-    socket.broadcast.emit('message', 'A new user has joined')
+        if (error) {
+            return callback(error)
+        }
+
+        socket.join(user.room)
+        socket.emit('message', generateMessage('Admin', 'Welcome!'))
+        socket.broadcast.to(user.room).emit('message', generateMessage('Admin', `${user.username} has joined!`))
+
+        io.to(user.room).emit('roomData', {
+            room: user.room,
+            users: getUsersInRoom(user.room)
+        })
+
+        callback()
+    })
 
     socket.on('sendMessage', (message, callback) => {
+        const user = getUser(socket.id)
         const filter = new Filter()
         if (filter.isProfane(message)) {
             return callback('Profanity is not allowed!')                
         }
 
-        io.emit('message', message)
-        //callback('Delivered!')
+        io.to(user.room).emit('message', generateMessage(user.username, message))
         callback()
     })
 
     socket.on('sendLocation', (coords, callback) => {
-        //io.emit('message', `Location: ${coords.latitude}, ${coords.longitude}`)
-        io.emit('message' ,`https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+        const user = getUser(socket.id)
+        io.to(user.room).emit('locationMessage' , generateLocationMessage(user.username, `https://google.com/maps?q=${coords.latitude},${coords.longitude}`))
         callback()
     })
 
+
+    socket.on('roomData', ({ room, users }) => {
+        
+    }) 
+
     socket.on('disconnect', () => {
         //io.emit('A user has left')
-        socket.broadcast.emit('message', 'A user has left')
+        const user = removeUser(socket.id)
+        if (user) {
+            io.to(user.room).emit('message', generateMessage('Admin', `${user.username} has left`))
+            io.to(user.room).emit('rootData', {
+                room: user.room,
+                users: getUsersInRoom(user.room)
+            })
+        }        
     })
 
 })
